95f9fe0b1cdf3d63649fd671098542ef93ffa9eb
grantschwab <114035150+grantschwab@users.noreply.github.com>
2025-07-10 16:07:06 -0400

    Update nws_map_clean.py

--- nws_map_clean.py
+++ nws_map_clean.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 # Load shapefile
-gdf = gpd.read_file("/Users/grantschwab/Desktop/nws_map/raw/nws_map_raw/z_18mr25.shp")
+gdf = gpd.read_file(INPUT_FILE_PATH)
 
 # Filter to STATE == 'MI'
 gdf_mi = gdf[gdf["STATE"] == "MI"]
@@ -16,4 +16,4 @@ gdf_dissolved.plot()
 plt.show()
 
 # Save to GeoJSON
-gdf_dissolved.to_file("/Users/grantschwab/Desktop/nws_map/output/mi_dissolved_by_cwa.geojson", driver="GeoJSON")
+gdf_dissolved.to_file(OUTPUT_FILE_PATH)
